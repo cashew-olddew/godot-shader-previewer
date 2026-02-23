b89a298bda2dfb9f568e2f99ae2adaf7cf2db6b8
cashew-olddew <cashewolddew@gmail.com>
2026-02-23 19:43:21 +0200

    Preview without uniform params and shader editor detection

--- addons/gdshader-partial-preview/gdshader_partial_preview.gd
+++ addons/gdshader-partial-preview/gdshader_partial_preview.gd
@@ -2,19 +2,20 @@
 extends EditorPlugin
 
 var dock: EditorDock = null
+var dock_scene: PartialPreviewDock = null
 var shader_code_editor: CodeEdit = null
+var current_selection: Node = null
 
 func _enable_plugin():
 	pass
 
 
 func _disable_plugin():
-	# Remove autoloads here.
 	pass
 
 
 func _enter_tree():
-	var dock_scene = preload("res://addons/gdshader-partial-preview/partial_preview_dock.tscn").instantiate()
+	dock_scene = preload("res://addons/gdshader-partial-preview/partial_preview_dock.tscn").instantiate()
 	
 	# Add new dock
 	dock = EditorDock.new()
@@ -28,7 +29,14 @@ func _enter_tree():
 	
 	get_shader_code_edit()
 	# TODO: find a way to update the shader_code_editor when new shader added or shader changed
-		
+	if shader_code_editor:
+		shader_code_editor.caret_changed.connect(_on_preview_try)
+	
+func _on_preview_try() -> void:
+	var caret_line_index = shader_code_editor.get_caret_line()
+	var caret_text: String = shader_code_editor.text
+	dock_scene.update_shader_preview(caret_text, caret_line_index)
+	
 func get_shader_code_edit() -> void:
 	# Currently there's no public API for getting the Shader Editor,
 	# so I get the internal ShaderEditor class to find it.
@@ -41,6 +49,7 @@ func get_shader_code_edit() -> void:
 			for ce in code_edits:
 				if ce.is_visible_in_tree():
 					shader_code_editor = ce
+					return
 	shader_code_editor = null
 
 func _exit_tree():
--- addons/gdshader-partial-preview/partial_preview_dock.gd
+++ addons/gdshader-partial-preview/partial_preview_dock.gd
@@ -1,2 +1,74 @@
 @tool
 extends ColorRect
+class_name PartialPreviewDock
+
+func update_shader_preview(text: String, current_line_index: int) -> void:
+	var generated_code = _generate_preview_shader(text, current_line_index)
+	
+	var shader_content := Shader.new()
+	shader_content.code = generated_code
+	
+	var shader_material := ShaderMaterial.new()
+	shader_material.shader = shader_content
+	
+	material = shader_material
+
+func _generate_preview_shader(original_code: String, line_index: int) -> String:
+	var lines = original_code.split("\n")
+	
+	if line_index >= lines.size():
+		return original_code
+
+	var current_line_text = lines[line_index]
+	
+	# Search for variable assignment
+	var var_regex = RegEx.new()
+	var_regex.compile(r"(\w+)\s*([+\-*/%]?=)")
+	var var_match = var_regex.search(current_line_text)
+	
+	if not var_match:
+		return original_code
+		
+	var var_name = var_match.get_string(1)
+	var type = _find_var_type(var_name, original_code, line_index)
+	
+	# All code before assignment stays as it was
+	var truncated_lines = []
+	for i in range(line_index + 1):
+		truncated_lines.append(lines[i])
+	
+	# Inject COLOR preview
+	var injection = ""
+	match type:
+		"float": injection = "COLOR = vec4(vec3(%s), 1.0);" % var_name
+		"vec2":  injection = "COLOR = vec4(%s, 0.0, 1.0);" % var_name
+		"vec3":  injection = "COLOR = vec4(%s, 1.0);" % var_name
+		"vec4":  injection = "COLOR = %s;" % var_name
+		_: return original_code
+		
+	truncated_lines.append(injection)
+	
+	# Close parantheses
+	var full_truncated_text = "\n".join(truncated_lines)
+	var open_braces = full_truncated_text.count("{")
+	var closed_braces = full_truncated_text.count("}")
+	var needed_closures = open_braces - closed_braces
+	
+	for i in range(needed_closures):
+		full_truncated_text += "\n}"
+		
+	return full_truncated_text
+
+func _find_var_type(var_name: String, full_code: String, line_index: int) -> String:
+	var lines = full_code.split("\n")
+	var type_regex = RegEx.new()
+	type_regex.compile("(float|vec2|vec3|vec4|int|bool)\\s+" + var_name + "\\b")
+	
+	for i in range(line_index, -1, -1):
+		var m = type_regex.search(lines[i])
+		if m: return m.get_string(1)
+			
+	if var_name in ["UV", "SCREEN_UV"]: return "vec2"
+	if var_name in ["COLOR", "MODULATE"]: return "vec4"
+	if var_name == "TIME": return "float"
+	return ""
