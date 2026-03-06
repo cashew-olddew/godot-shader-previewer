918c48c7dcf90c36b0852e0b7fc163b3feea3cb6
TheMagnat <guillaume.magniadas@gmail.com>
2026-03-06 03:20:20 +0100

    Added floating window option with resize, move and floating buttons that display on hover. Fixed bug where the default shader would still show with an error.

--- addons/shader-previewer/shader_previewer_dock.gd
+++ addons/shader-previewer/shader_previewer_dock.gd
@@ -1,6 +1,8 @@
 @tool
-extends TextureRect
+extends PanelContainer
 class_name ShaderLinePreviewerDock
+
+signal floating_requested
 
 const BUILTINS := {
 	[
@@ -71,29 +73,206 @@ const CANVAS_ASSIGNMENTS := {
 	"vec4": "COLOR = %s;",
 }
 
+@onready var preview: TextureRect = $Preview
+@onready var sub_viewport: SubViewport = $SubViewport
+@onready var top_button_container: HBoxContainer = $Preview/TopButtonContainer
+
 @export var viewport_texture: ViewportTexture
 @export var preview_mesh: MeshInstance3D
 @export var label: RichTextLabel
 @export var camera_3d: Camera3D
 
+@export var resize_view_button: Button
+@export var move_view_button: Button
+@export var floating_button: Button
+
 var _initial_texture: Texture2D = null
-var _mode_3d := false:
-	set(v):
-		_mode_3d = v
+var _mode_3d := false
+
+var _is_floating: bool = false
+var current_shader_code_editor: CodeEdit
+var _should_hide_buttons: bool = false # Used to prevent hiding during resize/move
 
 func _ready():
-	_initial_texture = texture
+	_initialize_theme_color()
+	
+	_initial_texture = preview.texture
+	
+	top_button_container.hide()
+	
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	
+	resize_view_button.button_down.connect(_resize_button_down)
+	resize_view_button.button_up.connect(_resize_button_up)
+	
+	move_view_button.button_down.connect(_move_button_down)
+	move_view_button.button_up.connect(_move_button_up)
+	
+	floating_button.pressed.connect(func(): floating_requested.emit())
+	
+	_last_floating_panel_size = size
 
+func _initialize_theme_color() -> void:
+	var style_box: StyleBoxFlat = get_theme_stylebox("panel")
+	
+	var root := EditorInterface.get_base_control()
+	var base_color = root.get_theme_color("base_color", "Editor")
+	
+	style_box.bg_color = base_color
+	style_box.border_color = base_color
+	(preview.texture as GradientTexture1D).gradient.set_color(0, base_color)
+
+func set_floating_mode(floating: bool) -> void:
+	_is_floating = floating
+	if floating:
+		resize_view_button.show()
+		move_view_button.show()
+		size = _last_floating_panel_size
+		set_anchors_and_offsets_preset(Control.PRESET_BOTTOM_RIGHT, Control.PRESET_MODE_KEEP_SIZE, 20)
+		
+		# This prevent the preview to keep a now outside old size/pos
+		if current_shader_code_editor: resize_to_editor_shape()
+	else:
+		resize_view_button.hide()
+		move_view_button.hide()
+
+func _on_mouse_entered() -> void:
+	top_button_container.show()
+	_should_hide_buttons = false
+
+func _on_mouse_exited() -> void:
+	if _in_resize or _in_move:
+		_should_hide_buttons = true
+		return
+	
+	top_button_container.hide()
+
+#region Resize and Move
+const MIN_SIZE := Vector2(150.0, 150.0)
+
+var _in_resize: bool = false
+var _in_move: bool = false
+
+var _initial_mouse_position: Vector2
+var _initial_panel_size: Vector2
+var _initial_panel_position: Vector2
+
+var _last_floating_panel_size: Vector2
+
+func _resize_button_down() -> void:
+	if not _is_floating: return
+	
+	_in_resize = true
+	_initial_mouse_position = get_global_mouse_position()
+	_initial_panel_size = size
+	_initial_panel_position = position
+
+func _resize_button_up() -> void:
+	if not _is_floating: return
+	_in_resize = false
+	if _should_hide_buttons:
+		top_button_container.hide()
+		_should_hide_buttons = false
+
+func _move_button_down() -> void:
+	if not _is_floating: return
+	
+	_in_move = true
+	_initial_mouse_position = get_global_mouse_position()
+	_initial_panel_position = position
+
+func _move_button_up() -> void:
+	if not _is_floating: return
+	
+	_in_move = false
+	if _should_hide_buttons:
+		top_button_container.hide()
+		_should_hide_buttons = false
+
+func _gui_input(event: InputEvent) -> void:
+	if not _is_floating: return
+	
+	if not _in_resize and not _in_move: return
+	
+	var mouse_move := event as InputEventMouseMotion
+	if not mouse_move: return
+	
+	var delta_mouse_position: Vector2 = _initial_mouse_position - mouse_move.global_position
+	
+	if _in_resize:
+		var resized_size: Vector2 = _initial_panel_size + delta_mouse_position
+		resized_size = resized_size.max(MIN_SIZE)
+		
+		var resized_pos: Vector2 = _initial_panel_position - (resized_size - _initial_panel_size)
+		
+		var code_edit: CodeEdit = get_parent() as CodeEdit
+		if code_edit:
+			if resized_pos.x < 0.0:
+				resized_size.x += resized_pos.x
+				resized_pos.x = 0.0
+			if resized_pos.y < 0.0:
+				resized_size.y += resized_pos.y
+				resized_pos.y = 0.0
+		
+		sub_viewport.size = resized_size
+		
+		size = resized_size
+		position = _initial_panel_position - (resized_size - _initial_panel_size)
+		
+		_last_floating_panel_size = size
+	
+	elif _in_move:
+		var moved_pos: Vector2 = _initial_panel_position - delta_mouse_position
+		
+		if current_shader_code_editor:
+			moved_pos = moved_pos.clamp(Vector2.ZERO, _get_current_max_pos())
+		
+		position = moved_pos
+
+func _get_current_max_size() -> Vector2:
+	return current_shader_code_editor.size - Vector2(
+			current_shader_code_editor.get_v_scroll_bar().size.x,
+			current_shader_code_editor.get_h_scroll_bar().size.y
+	)
+
+func _get_current_max_pos() -> Vector2:
+	return current_shader_code_editor.size - (
+		size + Vector2(
+			current_shader_code_editor.get_v_scroll_bar().size.x,
+			current_shader_code_editor.get_h_scroll_bar().size.y
+	))
+
+func resize_to_editor_shape() -> void:
+	var max_size: Vector2 = _get_current_max_size()
+	if max_size.x <= 0.0: return
+	
+	if max_size < MIN_SIZE:
+		#TODO: Add a collapsing feature if the remaining space in the shader code editor is too small
+		pass
+	
+	var new_size: Vector2 = size.clamp(MIN_SIZE, max_size)
+	
+	size = new_size
+	position = position.max(Vector2.ZERO)
+	
+	_last_floating_panel_size = size
+#endregion
+
+#region Shader Preview Core
 func _show_error(message: String) -> void:
 	label.text = message
-	material = null
-	texture = _initial_texture
+	preview.material = null
+	preview.texture = _initial_texture
 
 func update_shader_preview(text: String, current_line_index: int, selected_material: ShaderMaterial) -> void:
 	label.text = ""
-	var generated_code = _generate_preview_shader(text, current_line_index)
+	var generated_preview_result: Dictionary = _generate_preview_shader(text, current_line_index)
+	if not generated_preview_result["success"]:
+		return
+	
 	var shader_content := Shader.new()
-	shader_content.code = generated_code
+	shader_content.code = generated_preview_result["generated_code"]
 
 	# A new material is created to not overwrite the node's actual material
 	var preview_material = ShaderMaterial.new()
@@ -106,17 +285,17 @@ func update_shader_preview(text: String, current_line_index: int, selected_mater
 	_sync_material_parameters(selected_material, preview_material)
 
 	if _mode_3d:
-		material = null
-		texture = viewport_texture
+		preview.material = null
+		preview.texture = viewport_texture
 		preview_mesh.set_surface_override_material(0, preview_material)
 	else:
-		material = preview_material
+		preview.material = preview_material
 	
 func update_texture(node: Node) -> void:
 	if node and "texture" in node and node.texture:
-		texture = node.texture
+		preview.texture = node.texture
 	else:
-		texture = _initial_texture
+		preview.texture = _initial_texture
 
 # Helper function to check if two shaders correspond
 # Kind of hacky, as it just matches uniforms, but good enough for now
@@ -231,14 +410,16 @@ func _get_shader_type(code: String) -> String:
 	var result := regex.search(code)
 	return "error" if result == null else result.get_string(1)
 
-func _generate_preview_shader(original_code: String, line_index: int) -> String:
+func _generate_preview_shader(original_code: String, line_index: int) -> Dictionary:
+	var result: Dictionary = {"success": false, "generated_code": ""}
+	
 	var shader_type := _get_shader_type(original_code)
 	if shader_type == "error":
 		_show_error("No [b]shader_type[/b] statement found on shader")
-		return original_code
+		return result
 	if shader_type.is_empty():
 		_show_error("Preview only supports [b]canvas_item[/b] and [b]spatial[/b] shaders")
-		return original_code
+		return result
 	_mode_3d = shader_type == "spatial"
 	
 	var lines = original_code.split("\n")
@@ -246,17 +427,17 @@ func _generate_preview_shader(original_code: String, line_index: int) -> String:
 	
 	if enclosing_function != "fragment":
 		_show_error("Preview only supports assignments in the [b]fragment()[/b] function")
-		return original_code
+		return result
 	
 	if line_index >= lines.size():
 		_show_error("Something weird happened... Try restarting the plugin.")
-		return original_code
+		return result
 		
 	var stmt = _find_statement(lines, line_index)
 	
 	if stmt.is_empty():
 		_show_error("The selected line needs to be an assignment.")
-		return original_code
+		return result
 		
 	var var_name: String = stmt["var_name"]
 	var last_line: int = stmt["end"]
@@ -272,7 +453,7 @@ func _generate_preview_shader(original_code: String, line_index: int) -> String:
 	var assignments: Dictionary = SPATIAL_ASSIGNMENTS if _mode_3d else CANVAS_ASSIGNMENTS
 	if not assignments.has(type):
 		_show_error("Preview unavailable for current assignment.\nSupported types are: [b]bool, int, float, vec2, vec3, vec4[/b].")
-		return original_code
+		return result
 	
 	injection = assignments[type] % var_name
 	
@@ -286,8 +467,10 @@ func _generate_preview_shader(original_code: String, line_index: int) -> String:
 	
 	for i in range(needed_closures):
 		full_truncated_text += "\n}"
-		
-	return full_truncated_text
+	
+	result["success"] = true
+	result["generated_code"] = full_truncated_text
+	return result
 
 func _find_var_type(var_name: String, full_code: String, line_index: int) -> String:
 	var lines = full_code.split("\n")
@@ -312,3 +495,4 @@ func _find_var_type(var_name: String, full_code: String, line_index: int) -> Str
 			return BUILTINS[builtin_collection]
 	
 	return ""
+#endregion
